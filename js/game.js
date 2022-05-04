46c87c87d7e4ad53d6cc129cda2a07d9e6626b2e
Bartek Swiderski <b.swiderski92@gmail.com>
2022-05-04 13:09:40 +0100

    rename normal to medium

--- js/game.js
+++ js/game.js
@@ -9,7 +9,7 @@ class Game extends UI {
             cols: 8,
             mines: 10
         },
-        normal: {
+        medium: {
             rows: 16,
             cols: 16,
             mines: 40
